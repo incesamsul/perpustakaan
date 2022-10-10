33fd65ca8b0074c049f1171f1191d931c8350c23
incesamsul <incesamsul89@gmail.com>
2022-10-11 07:50:37 +0800

    fix wa

--- resources/views/pages/denda/index.blade.php
+++ resources/views/pages/denda/index.blade.php
@@ -28,7 +28,7 @@
                 <td>Tgl kembali</td>
                 <td>Terlambat</td>
                 <td>Denda</td>
-                
+
                 <td>Aksi</td>
             </tr>
             @foreach ($buku as $row)
@@ -50,9 +50,11 @@
                     <td class="align-middle">{{ $jmlTerlambat < 0 ? '0' : $jmlTerlambat }} Hari</td>
                     <td class="align-middle">{{ $jmlTerlambat < 0 ? '0' : 'Rp. '. number_format($jmlTerlambat * 500) }} </td>
                     <td>
-                        <?php 
+                        <?php
                             $noHp = $row->user->member->no_hp;
-                            $message = $row->user->name . 'Bukunya di kembalikan dong kalo pinjam';
+                            $terlambat = $jmlTerlambat < 0 ? '0' : $jmlTerlambat;
+                            $denda = $jmlTerlambat < 0 ? '0' : 'Rp. '. number_format($jmlTerlambat * 500);
+                            $message = $row->user->name . 'Anda belum mengembalikan Buku perpustakaan sudah terlambat ' . $terlambat . ' hari dari tanggal ' . $row->tgl_pinjam . '  denda sebanyak  Rp '. $denda .' * dari Admin Perpustakaan*';
                         ?>
                         <a href="https://web.whatsapp.com/send?phone={{ $noHp }}&text={{ $message }}" target="_blank" class="btn btn-success"><i class="fab fa-whatsapp"></i></button>
                     </td>
--- resources/views/pages/pinjamkan/index.blade.php
+++ resources/views/pages/pinjamkan/index.blade.php
@@ -32,6 +32,7 @@
                             <tr>
                                 <th width="5%" class="sorting" data-sorting_type="asc" data-column_name="id" style="cursor: pointer">ID <span id="id_icon"></span></th>
                                 <td>Judul</td>
+                                <td>Nama</td>
                                 <td>Pengarang</td>
                                 <td>Tahun Terbit</td>
                                 <td>Penerbit</td>
@@ -44,6 +45,7 @@
                                 <tr>
                                     <td>{{ $loop->iteration }}</td>
                                     <td>{{ $row->buku->judul }}</td>
+                                    <td>{{ $row->user->name }}</td>
                                     <td>{{ $row->buku->pengarang }}</td>
                                     <td>{{ $row->buku->tahun_terbit }}</td>
                                     <td>{{ $row->buku->penerbit }}</td>
